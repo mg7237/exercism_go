8a3e264dce141ffdb7b2db16d1a3d14f85f11d00
Easyleases Admin <admin@easyleases.in>
2020-10-08 12:35:11 +0530

    tournament solution updated as per mentor comments

--- tournament/tournament.go
+++ tournament/tournament.go
@@ -9,123 +9,89 @@ import (
 	"strings"
 )
 
-type club struct {
-	name   string
+type clubScore struct {
 	win    int
 	loss   int
 	draw   int
 	points int
 }
 
-var clubs []club
+type sortClub struct {
+	name   string
+	points int
+}
+
+var club map[string]*clubScore
 var err error
 
 // Tally generates soccer tournament tally
 func Tally(reader io.Reader, buffer io.Writer) error {
-	var results []string
+	club = map[string]*clubScore{}
 	rd := bufio.NewReader(reader)
 
 	for {
 		line, err := rd.ReadString('\n')
 
 		if err == io.EOF {
-			results = append(results, line)
+			if err = fillMap(line); err != nil {
+				return err
+			}
 			break
 		}
-		if err != nil {
+
+		if err = fillMap(line); err != nil {
 			return err
 		}
-		results = append(results, line)
 	}
-	for _, result := range results {
-		if len(result) > 1 && string(result[0]) != "#" {
-			s := strings.Split(result, ";")
-			j, ok := findClub(s[0])
-			if !ok {
-				clubs = append(clubs, club{
-					name: s[0],
-					win:  0,
-					loss: 0,
-					draw: 0,
-				})
-				j = len(clubs) - 1
-			}
 
-			err = update(j, s)
-			if err != nil {
-				return err
-			}
-
-			k, ok := findClub(s[1])
-			if !ok {
-				clubs = append(clubs, club{
-					name: s[1],
-					win:  0,
-					loss: 0,
-					draw: 0,
-				})
-				k = len(clubs) - 1
-			}
-			err = update(k, s)
-			if err != nil {
-				return err
-			}
-		}
+	sortC := make([]sortClub, 0, len(club))
+	for k := range club {
+		sortC = append(sortC, sortClub{name: k, points: club[k].points})
 	}
-	sortClubs()
+	sortClubs(sortC)
+
 	_, err := io.WriteString(buffer, "Team                           | MP |  W |  D |  L |  P\n")
 	if err != nil {
 		return err
 	}
-	for _, club := range clubs {
-		s := fmt.Sprintf("%-31v|%3v |%3v |%3v |%3v |%3v\n", club.name, club.win+club.loss+club.draw, club.win, club.draw, club.loss, club.points)
-		_, err := io.WriteString(buffer, s)
-		if err != nil {
+	for _, c := range sortC {
+		s := fmt.Sprintf("%-31v|%3v |%3v |%3v |%3v |%3v\n", c.name, club[c.name].win+club[c.name].loss+club[c.name].draw, club[c.name].win, club[c.name].draw, club[c.name].loss, club[c.name].points)
+		if _, err := io.WriteString(buffer, s); err != nil {
 			return err
 		}
 	}
-	clubs = nil
 	return nil
 }
-func update(i int, r []string) error {
+
+func update(c string, r []string) error {
 	if len(r) < 3 {
 		return errors.New("Incomplete result")
 	}
 
 	result := strings.Replace(r[2], "\n", "", -1)
-
-	if clubs[i].name == r[0] && result == "win" {
-		clubs[i].win++
-		clubs[i].points = clubs[i].points + 3
+	if c == r[0] && result == "win" {
+		club[c].win++
+		club[c].points += 3
 		return nil
-	} else if clubs[i].name == r[0] && result == "loss" {
-		clubs[i].loss++
+	} else if c == r[0] && result == "loss" {
+		club[c].loss++
 		return nil
-	} else if clubs[i].name == r[1] && result == "win" {
-		clubs[i].loss++
+	} else if c == r[1] && result == "win" {
+		club[c].loss++
 		return nil
-	} else if clubs[i].name == r[1] && result == "loss" {
-		clubs[i].win++
-		clubs[i].points = clubs[i].points + 3
+	} else if c == r[1] && result == "loss" {
+		club[c].win++
+		club[c].points += 3
 		return nil
 	} else if result == "draw" {
-		clubs[i].draw++
-		clubs[i].points++
+		club[c].draw++
+		club[c].points++
 		return nil
 	}
 	return errors.New("No if condition captured")
 }
 
-func findClub(val string) (int, bool) {
-	for i, club := range clubs {
-		if club.name == val {
-			return i, true
-		}
-	}
-	return -1, false
-}
-
-func sortClubs() {
+func sortClubs(clubs []sortClub) {
 	sort.SliceStable(clubs, func(i, j int) bool {
 		ci, cj := clubs[i], clubs[j]
 		switch {
@@ -135,4 +101,26 @@ func sortClubs() {
 			return ci.name < cj.name
 		}
 	})
+}
+
+func fillMap(result string) error {
+
+	if len(result) > 1 && string(result[0]) != "#" {
+		s := strings.Split(result, ";")
+		if _, ok := club[s[0]]; !ok {
+			club[s[0]] = &clubScore{}
+		}
+		if err = update(s[0], s); err != nil {
+			return err
+		}
+
+		if _, ok := club[s[1]]; !ok {
+			club[s[1]] = &clubScore{}
+		}
+
+		if err = update(s[1], s); err != nil {
+			return err
+		}
+	}
+	return nil
 }

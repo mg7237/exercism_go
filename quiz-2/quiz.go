642e1d7524f05537b11a65e047291c08234cfc8b
Easyleases Admin <admin@easyleases.in>
2020-10-20 13:08:26 +0530

    Quiz V2

--- quiz-2/quiz.go
+++ quiz-2/quiz.go
@@ -6,8 +6,11 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 )
+
+var count, lenqa, question int = 0, 0, 1
 
 func main() {
 
@@ -22,11 +25,10 @@ func main() {
 		fmt.Println("Invalid File:", f)
 		os.Exit(2)
 	}
-	timer(l)
+	go timer(l)
 	reader := bufio.NewReader(os.Stdin)
-	count, qestion := 0, 1
 	for i := range qna {
-		fmt.Println("Question #", qestion, ": ", qna[i].q, "=")
+		fmt.Println("Question #", question, ": ", qna[i].q, "=")
 		text, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Println("Error reading input", f)
@@ -35,9 +37,9 @@ func main() {
 		if text == qna[i].a+"\n" {
 			count++
 		}
-		qestion++
+		question++
 	}
-	fmt.Println("Your Score: ", count, " out of ", len(qna))
+	fmt.Println("Your Score: ", count, " out of ", lenqa)
 }
 
 type quiz struct {
@@ -59,13 +61,16 @@ func readCSV(file string) (map[int]quiz, error) {
 	for i, line := range lines {
 		qna[i] = quiz{line[0], line[1]}
 	}
-
+	lenqa = len(qna)
 	return qna, nil
 }
 
-func timer(t int) {
-	var d duration
-	d.Seconds(t)
-	timer := time.NewTimer(t * time.Second)
-
+func timer(seconds int) {
+	s, _ := time.ParseDuration(strconv.Itoa(seconds) + "s")
+	t := time.NewTimer(s)
+	<-t.C
+	if question != lenqa {
+		fmt.Println("\nYour Score: ", count, " out of ", lenqa)
+	}
+	os.Exit(0)
 }
